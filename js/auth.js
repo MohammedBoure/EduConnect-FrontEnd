a2400391ae1d4f9a2349adc03a0aeafc859fce01
MohammedBoure <mohamdbormoz2@gmail.com>
2025-04-26 11:49:07 +0200

    fix err login in file js/auth.js with var API_BASE_URL

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1:5000/api'; // أو 'https://educonnect-wp9t.onrender.com/api' للإنتاج
+const API_BASE_URL = 'https://educonnect-wp9t.onrender.com/api/api'; // أو 'https://educonnect-wp9t.onrender.com/api' للإنتاج
 
 function setToken(token) {
     if (!token) {
